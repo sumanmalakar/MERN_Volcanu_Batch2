5772f101103ac0559ec34f9e5597170045faa239
“sumanmalakar” <malakars041@gmail.com>
2024-03-20 13:17:07 +0530

    useState Completed

--- day50_React/src/App.jsx
+++ day50_React/src/App.jsx
@@ -1,18 +1,60 @@
-import Hero from "./Hero";
-import Laptop from "./Laptop";
-import Phone from "./Phone";
-import Tablet from "./Tablet";
-import Person from "./components/Person";
+import Phone from "./components/Phone";
+import { useState } from "react";
 const App = () => {
+  // const laptop = {
+  //   name: "hp probook 430 g2",
+  //   price: 450000,
+  //   processor: "Intel i5 6th gen",
+
+  // state variable
+
+  const [counter,setCounter] = useState(10)
+
+  const light = () => {
+    console.log("btn is cliked");
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.color = "black";
+  };
+  const dark = () => {
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.color = "white";
+  };
+
+  // setCounter("batman")
+
+  const increaseBy1 = () => {
+    console.log("Before Increase Counter = ", counter);
+    // counter = counter +1
+    // counter++;
+    setCounter(counter+1)
+    console.log("After Increase Counter = ", counter);
+  };
+
+  const decreaseBy1 = () => {
+    console.log("Before Decrease Counter = ", counter);
+    // counter = counter -1
+    // counter--;
+    setCounter(counter-1)
+    console.log("After Decrease Counter = ", counter);
+  };
+
+// hooks 
+// useState(), useEffect(), useMemo(), useReducer(), useContext, useCollback, useLayoutEffect, 
+
   return (
     <>
-      {/* <Tablet />
-      <Phone />
-      <Hero />
-      <Laptop /> */}
-      <Person name={"superman"} />
-      <Person name={"spiderman"} />
-      <Person name={"batman"} />
+    <button onClick={()=>setCounter(counter+1)}>Incrase</button>
+    <button onClick={()=>setCounter(counter-1)}>Decrease</button>
+      <Phone
+        ram={"100gb"}
+        name={"pocco"}
+        price={"5000"}
+        light={light}
+        dark={dark}
+        counter={counter}
+        increase={increaseBy1}
+        decrease={decreaseBy1}
+      />
     </>
   );
 };
