5fab7187e082dfbe539a0bf55f1f519f456f723a
sumanmalakar <malakars041@gmail.com>
2024-06-21 12:49:19 +0530

    adding middleware

--- 01_MERN/API/server.js
+++ 01_MERN/API/server.js
@@ -2,25 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "express";
 import productRouter from "./Routes/product.js";
-import cors from 'cors'
+import userRouter from "./Routes/user.js";
+import cors from "cors";
 
 const app = express();
-
 app.use(bodyParser.json());
-
 app.use(
   cors({
     origin: true,
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
+// user router
+app.use("/api/user", userRouter);
+
 // product router
 app.use("/api/product", productRouter);
-
-
-
 
 mongoose
   .connect(
